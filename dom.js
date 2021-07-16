937bad0af42dd8a7949d4fc0b2d39e897642e45d
somani290 <2020UCP1982@mnit.ac.in>
2021-07-16 12:39:08 +0530

    commit

---
+++ dom.js
@@ -0,0 +1,70 @@
+
+var list1 = document.getElementById('1');
+list1.addEventListener('click',play1);
+function play1() {
+    var a = document.getElementById('A');
+    a.play();
+}
+
+var list2 = document.getElementById('2');
+list2.addEventListener('click',play2);
+function play2() {
+    var b = document.getElementById('B');
+    b.play();
+}
+
+var list3 = document.getElementById('3');
+list3.addEventListener('click',play3);
+function play3() {
+    var c = document.getElementById('C');
+    c.play();
+}
+
+var list4 = document.getElementById('4');
+list4.addEventListener('click',play4);
+function play4() {
+    var d = document.getElementById('D');
+    d.play();
+}
+
+var list5 = document.getElementById('5');
+list5.addEventListener('click',play5);
+function play5() {
+    var e = document.getElementById('E');
+    e.play();
+}
+
+var list6 = document.getElementById('6');
+list6.addEventListener('click',play6);
+function play6() {
+    var f = document.getElementById('F');
+    f.play();
+}
+
+var list7 = document.getElementById('7');
+list7.addEventListener('click',play7);
+function play7() {
+    var g = document.getElementById('G');
+    g.play();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
